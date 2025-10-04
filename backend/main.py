754cc68b4c90a213b00801ac96be0836706afc1c
Daniel <daniel.cermann@gmx.de>
2025-10-04 18:16:00 +0200

    implemented basic Incident log storage

--- backend/main.py
+++ backend/main.py
@@ -1,34 +1,60 @@
 from flask import Flask
-from flask_restx import Api, Resource
+from datetime import datetime
+from flask_restx import Api, Resource, fields
+from database.db import db
+from database.IncidentLogModel import IncidentLogModel
 
-app = Flask(__name__)
-api = Api(app, version="1.0", title="My API",
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    api = Api(app, version="1.0", title="My API",
           description="Decoy app backend api")
+    ns = api.namespace("IncidentLogs", description="IncidentLogs operations")
 
-alerts = api.namespace("alerts", description="Alerts operations")
+    incident_log_model = api.model("IncidentLog", {
+        "id": fields.Integer(readonly=True),
+        "title": fields.String(required=True, description="The incident title"),
+        "category": fields.String(required=False, description="The incident category (network, hardware, software, other)"),
+        "description": fields.String(required=False, description="The incident description"),
+        "timestamp": fields.DateTime(required=False, description="The incident timestamp as unix timestamp"),
+        "severity": fields.String(required=False, description="The incident severity (critical, moderate, low)"),
+    })
 
-ITEMS = []
+    @ns.route("/")
+    class IncidentLogList(Resource):
+        @ns.marshal_list_with(incident_log_model)
+        def get(self):
+            return IncidentLogModel.query.all()
 
-@alerts.route("/")
-class AlertList(Resource):
-    def get(self):
-        """List all alerts"""
-        return ITEMS
+        @ns.expect(incident_log_model)
+        @ns.marshal_with(incident_log_model, code=201)
+        def post(self):
+            data = api.payload
+            try:
+                if "timestamp" in data:
+                    timestamp = datetime.fromtimestamp(int(data.get("timestamp")))
+            except Exception as e:
+                timestamp = datetime.utcnow()
+            severity = data.get("severity")
+            category = data.get("category")
+            description = data.get("description")
+            title = data.get("title")
+            new_item = IncidentLogModel(title=title, timestamp=timestamp, severity=severity, category=category, description=description)
+            db.session.add(new_item)
+            db.session.commit()
+            return new_item, 201
 
-    def post(self):
-        """Create a new alert"""
-        item = {"id": len(ITEMS) + 1, "name": f"Alert {len(ITEMS)+1}"}
-        ITEMS.append(item)
-        return item, 201
+    return app
 
-@alerts.route("/<int:id>")
-class Alert(Resource):
-    def get(self, id):
-        """Get an alert by ID"""
-        for item in ITEMS:
-            if item["id"] == id:
-                return item
-        api.abort(404, f"Alert {id} not found")
 
 if __name__ == "__main__":
+    app = create_app()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
