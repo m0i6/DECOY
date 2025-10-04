fa76b2c1d97c4fa399ec2b8ff2fc4c17d2e125c2
jafber <spam@jan-berndt.de>
2025-10-05 01:06:12 +0200

    remove vite proxy from prod

--- frontend/src/backendProxy.ts
+++ frontend/src/backendProxy.ts
@@ -1,13 +1,14 @@
 import type { HoneypotType, IncidentLogType } from "./types"
+import { BACKEND_ROOT_URL } from "./config"
 
 export async function getHoneypots() {
-    const res = await fetch("api/HoneyPots")
+    const res = await fetch(BACKEND_ROOT_URL + "/HoneyPots")
     const json = await res.json()
     return json as HoneypotType[]
 }
 
 export async function getIncidentLogs() {
-    const res = await fetch("api/IncidentLogs")
+    const res = await fetch(BACKEND_ROOT_URL + "/IncidentLogs")
     const json = await res.json()
     return json as IncidentLogType[]
 }
