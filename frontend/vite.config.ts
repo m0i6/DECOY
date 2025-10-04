9f95b7ca07a4ae65d4f0f049ce02cd9d36840e32
jafber <spam@jan-berndt.de>
2025-10-05 00:53:24 +0200

    implement vite proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  // Load env vars from `.env`, `.env.development`, etc.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [vue()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_ROOT_URL || 'http://localhost:5000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  }
 })
