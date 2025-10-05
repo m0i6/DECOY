ecd74df9e07b074ee1cbcbac9bfdea7b4efaa981
jafber <spam@jan-berndt.de>
2025-10-05 02:12:12 +0200

    cors gewichse ein fuer alle mal gefixt

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,14 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig(({ mode }) => {
-  // Load env vars from `.env`, `.env.development`, etc.
-  const env = loadEnv(mode, process.cwd(), '')
-
+export default defineConfig(() => {
   return {
     plugins: [vue()],
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_BACKEND_ROOT_URL || 'http://localhost:5000',
+          target: 'http://localhost:5000',
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
         },
       },
     },
