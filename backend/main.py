18ce4701ddd160d7e75da265f2e5ce871f2306f8
jafber <spam@jan-berndt.de>
2025-10-05 02:12:12 +0200

    cors gewichse ein fuer alle mal gefixt

--- backend/main.py
+++ backend/main.py
@@ -8,14 +8,14 @@ from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+    CORS(app, origins=["http://localhost:5173"])
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
 
     api = Api(app, version="1.0", title="My API",
-          description="Decoy app backend api")
+          description="Decoy app backend api", prefix="/api")
 
     setup_incident_routes(api)
     setup_honeypot_routes(api)
